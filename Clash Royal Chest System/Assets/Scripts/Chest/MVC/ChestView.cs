923a43d31ec5a022cfd3387129da3b5f12acc0c6
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-08-05 15:17:31 +0530

    added var to chest view

--- Clash Royal Chest System/Assets/Scripts/Chest/MVC/ChestView.cs
+++ Clash Royal Chest System/Assets/Scripts/Chest/MVC/ChestView.cs
@@ -1,9 +1,22 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
 public class ChestView : MonoBehaviour
 {
-    
+    private ChestController chestController;
+    private ChestModel chestModel;
+    private ChestService chestService ;
+
+    private void Awake()
+    {
+        SetModelAndController();
+    }
+
+    private void SetModelAndController()
+    {
+        //private int sObjInt = Random.Range(0,);
+    }
 }
